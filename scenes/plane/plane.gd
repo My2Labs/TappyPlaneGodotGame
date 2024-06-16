4abcf6562d307ced26809b652a5c43c991b3e0b5
Sharron H <sharronhemenway@outlook.com>
2024-06-16 16:56:30 -0500

    Add plane animation

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
-const GRAVITY: float = 700.0
+const GRAVITY: float = 880.0
+const POWER: float = -400.0
 
+@onready var sprite: AnimatedSprite2D = $Sprite
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,7 +13,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	
 	velocity.y += GRAVITY * delta
 	
+	fly()
+	
 	move_and_slide()
+	
+	if is_on_floor() == true:
+		die()
+
+func fly() -> void:
+	if Input.is_action_just_pressed("fly") == true:
+		velocity.y = POWER
+		animation_player.play("power")
+
+func die() -> void:
+	sprite.stop()
+	set_physics_process(false)
