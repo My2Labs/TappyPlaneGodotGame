6d1da39f996501ec878cf9713a6bcd9fa9b8c0c1
Sharron H <sharronhemenway@outlook.com>
2024-06-16 17:54:45 -0500

    Add pipes and signals

---
+++ scenes/pipes/pipes.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+const SCROLL_SPEED: float = 120.0
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x -= SCROLL_SPEED * delta
+	
+	
+
+func _on_screen_exited():
+	print("_on_screen_exited")
+	queue_free() 
